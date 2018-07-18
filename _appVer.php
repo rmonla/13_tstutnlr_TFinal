527a3ca9be9a4816318213752f5e5ef88e05d22e
rmonla <rmonla@gmail.com>
2018-07-17 22:10:01 -0300

    v_1.2.2 Se agrega _appVer.php para el detalle de versiones.

---
+++ _appVer.php
@@ -0,0 +1,37 @@
+<?php  
+	/**
+	 * rmEdulic - Proyecto Final de Tecnicatura Superior en Programación.
+	 *
+	 *     EduLic, es un sistema de Gestión de Novedades y Consultas de Liquidaciones, 
+	 * desarrollado por el Grupo Binarios, para el Área de Liquidaciones del 
+	 * Ministerio de Educación, Ciencia y Tecnología de la Provincia de La Rioja.
+	 * 
+	 * @package rmEdulic
+	 * @see https://github.com/rmonla/rmEdulic The rmEdulic GitHub project
+	 * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+	 * @note This program is distributed in the hope that it will be useful - WITHOUT
+	 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+	 * FITNESS FOR A PARTICULAR PURPOSE.
+	 */
+
+	/**
+	 * v_1.1. En esta versión se reúnen todos los backups del momento cuando estaba en 
+	 * 			desarrollo inicial el proyecto.
+	 * 
+	 * @copyright 2012 - 2013 Miriam CANACHI
+	 *            			  Gastón FRANCISCO
+	 *            			  Ricardo MONLA
+	 *            			  Abel SLAZAR
+	 */
+
+	/**
+	 * 
+	 * @author Ricardo MONLA <rmonla@gmail.com>
+	 * @copyright 2018 - 2018 Ricardo MONLA
+	 * 
+	 * v_1.2.1 Inicia de actualización del sistema.
+	 *
+	 * 
+	 */
+
+?>
